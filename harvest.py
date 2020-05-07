7ece928a562cd2542305bdac3d0cb1ad52bd3bf4
Erica Chesley <erchesley@gmail.com>
2020-05-07 12:36:50 -0700

    Part 2 complete

--- harvest.py
+++ harvest.py
@@ -83,6 +83,17 @@ def make_melon_type_lookup(melon_types):
 class Melon(object):
     """A melon in a melon harvest."""
 
+    def __init__(self, melon_type, shape_rating, color_rating, field, harvester):
+        self.type = melon_type
+        self.shape_rating = shape_rating
+        self. color_rating = color_rating
+        self.field = field
+        self.harvester = harvester
+
+    def is_sellable(self):
+        return self.shape_rating > 5 and self.color_rating > 5 \
+        and self.field != 3
+
     # Fill in the rest
     # Needs __init__ and is_sellable methods
 
@@ -90,11 +101,49 @@ def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
     # Fill in the rest
+    melon_lookup = make_melon_type_lookup(melon_types)
+    melons = []
+
+    melon1 = Melon(melon_lookup['yw'], 8, 7, 2, 'Sheila')
+    melons.append(melon1)
+
+    melon2 = Melon(melon_lookup['yw'], 3, 4, 2, 'Sheila')
+    melons.append(melon2)
+
+    melon3 = Melon(melon_lookup['yw'], 9, 8, 3, 'Sheila')
+    melons.append(melon3)
+
+    melon4 = Melon(melon_lookup['cas'], 10, 6, 35, 'Sheila')
+    melons.append(melon4)
+
+    melon5 = Melon(melon_lookup['cren'], 8, 9, 35, 'Michael')
+    melons.append(melon5)
+
+    melon6 = Melon(melon_lookup['cren'], 8, 2, 35, 'Michael')
+    melons.append(melon6)
+
+    melon7 = Melon(melon_lookup['cren'], 2, 3, 4, 'Michael')
+    melons.append(melon7)
+
+    melon8 = Melon(melon_lookup['musk'], 6, 7, 4, 'Michael')
+    melons.append(melon8)
+
+    melon9 = Melon(melon_lookup['yw'], 7, 10, 3, 'Sheila')
+    melons.append(melon9)
+
+    return melons
+    
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
     # Fill in the rest 
+    for melon in melons:
+        if melon.is_sellable():
+            sellability = '(CAN BE SOLD)'
+        else:
+            sellability = '(NOT SELLABLE)'
+        print(f'Harvested by {melon.harvester} from Field {melon.field} {sellability}')
 
 
 
